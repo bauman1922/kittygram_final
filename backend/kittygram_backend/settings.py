1d8ac11e89e42bb9149686d6a58c2ca014596fc4
Сергей Саморуков <bauman1922@gmail.com>
2023-08-20 11:23:28 +0300

    test

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,17 +1,20 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+# load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+# SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "default_allowed_hosts").split(",")
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "default_allowed_hosts").split(",")
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
