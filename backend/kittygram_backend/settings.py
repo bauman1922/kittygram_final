eefcc51d3127ba6f707451214c620b5076443a7a
Сергей Саморуков <bauman1922@gmail.com>
2023-08-21 11:26:36 +0300

    correct settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,4 +1,5 @@
 import os
+import re
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -11,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = re.split(r'\s*,\s*', os.getenv('ALLOWED_HOSTS', default='*'))
 
 
 INSTALLED_APPS = [
