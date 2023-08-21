5be57046cd312e6f50e6d9ceb9b3c4ba1c642724
Сергей Саморуков <bauman1922@gmail.com>
2023-08-21 12:51:08 +0300

    settings new

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['158.160.31.142', '127.0.0.1', 'localhost', 'red-white.hopto.org']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 
 INSTALLED_APPS = [
